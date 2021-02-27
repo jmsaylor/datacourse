bf411eb7085660a040d7afa04189f62360b7a07a
jmsaylor <johnmsaylor@gmail.com>
2021-02-26 21:38:56 -0500

    data preprocessing in R

--- preprocessing/data_preprocessing.R
+++ preprocessing/data_preprocessing.R
@@ -1,23 +1,9 @@
- dataset = read.csv("Data.csv")
- 
- dataset$Age = ifelse(is.na(dataset$Age),
-                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
-                      dataset$Age)
-                      
- dataset$Salary = ifelse(is.na(dataset$Salary),
-                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                      dataset$Salary)
- 
- dataset$Country = factor(dataset$Country,
-                          levels = c('France', 'Spain', 'Germany'),
-                          labels = c(1, 2, 3))
- 
- dataset$Purchased = factor(dataset$Purchased,
-                            levels = c('No', 'Yes'),
-                            labels = c(0, 1))
+#install.packages('caTools')
+library(caTools) 
+dataset = read.csv("Data.csv")
 
- #install.packages('caTools') 
- library(caTools)
+# dataset = dataset[,2:3]
+
 set.seed(123)
 split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
@@ -25,3 +11,20 @@ test_set = subset(dataset, split == FALSE)
 
 training_set[, 2:3] = scale(training_set[, 2:3])
 test_set[, 2:3] = scale(test_set[, 2:3])
+
+
+# dataset$Age = ifelse(is.na(dataset$Age),
+#                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+#                      dataset$Age)
+# 
+# dataset$Salary = ifelse(is.na(dataset$Salary),
+#                         ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+#                         dataset$Salary)
+# 
+# dataset$Country = factor(dataset$Country,
+#                          levels = c('France', 'Spain', 'Germany'),
+#                          labels = c(1, 2, 3))
+# 
+# dataset$Purchased = factor(dataset$Purchased,
+#                            levels = c('No', 'Yes'),
+#                            labels = c(0, 1))
