4fa94e8c61fbc6d7344bdd9aca4df6a06b1ceb1a
jmsaylor <johnmsaylor@gmail.com>
2021-03-09 23:35:43 -0500

    logistic regression

---
+++ logistic_regression/main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn.model_selection
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+scale_x = StandardScaler().fit(X_train)
+X_train = scale_x.transform(X_train)
+X_test = scale_x.transform(X_test)
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
